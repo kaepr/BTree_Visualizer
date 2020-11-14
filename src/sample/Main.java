84339c77eb68e37e54d7c5694cb95ad17c5a9db0
Shagun Agrawal <agrawalshagun07@gmail.com>
2020-11-15 00:09:34 +0530

    Just fixing some stuff

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,15 +5,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.animation.RotateTransition;
+import javafx.application.Application;
+import javafx.scene.shape.Polygon;
+
+import static javafx.application.Application.launch;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("B Tree Visualizer");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.setWidth(1000);
+        primaryStage.setHeight(700);
+//        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
