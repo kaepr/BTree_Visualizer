2da9df699d4a08ad0d96e9bb24b5f653b94457fb
Shagun Agrawal <agrawalshagun07@gmail.com>
2020-11-16 01:48:01 +0530

    yes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.animation.RotateTransition;
 import javafx.application.Application;
@@ -21,12 +23,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setScene(new Scene(root, 300, 275));
+
         primaryStage.setTitle("B Tree Visualizer");
 
         primaryStage.setWidth(1000);
         primaryStage.setHeight(700);
-//        primaryStage.setScene(new Scene(root, 300, 275));
+        VBox root = new VBox();
+
+        Label label1 = new Label("This is a text label");
+        root.getChildren().add(label1);
+
+        Scene scene1 = new Scene(root);
+
+
+        primaryStage.setScene(scene1);
         primaryStage.show();
 
     }
